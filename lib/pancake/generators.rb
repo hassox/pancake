ae6481a61111e1d6dbcb3b437dc5817c684f135f
Glenn Davy <glenn@balder.(none)>
2009-07-21 12:17:34 +1000

    because base.rb isn't first generator loaded, others can faile to be required as they are instances of Base.

--- lib/pancake/generators.rb
+++ lib/pancake/generators.rb
@@ -1,4 +1,8 @@
 require 'thor'
 require 'extlib'
 
-Dir[File.join(File.dirname(__FILE__), "generators", "*.rb")].each{|f| require f}
+require File.join(File.dirname(__FILE__), "generators", "base.rb")
+
+Dir[File.join(File.dirname(__FILE__), "generators", "*.rb")].each do |f|
+ require f unless f == 'base.rb'
+end
