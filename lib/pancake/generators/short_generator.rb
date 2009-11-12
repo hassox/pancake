096d932cf2f9c8fe5e78c2848c6def58607d15d4
Daniel Neighman <has.sox@gmail.com>
2009-11-12 11:40:21 +1100

    Version bump to 0.1.16

--- lib/pancake/generators/short_generator.rb
+++ lib/pancake/generators/short_generator.rb
@@ -2,16 +2,20 @@ module Pancake
   module Generators
     class Short < Base
       argument :stack_name, :banner => "Name of stack"
-      
+
       desc "Generates a short stack"
       def stack
         say "Creating The Short Stack For #{stack_name}"
         directory "short/%stack_name%", stack_name
         template  File.join(self.class.source_root, "common/dotgitignore"), "#{stack_name}/.gitignore"
         template  File.join(self.class.source_root, "common/dothtaccess"),  "#{stack_name}/lib/#{stack_name}/public/.htaccess"
+
+        inside("#{stack_name}/lib/#{stack_name}/script") do
+          run 'chmod +x console'
+        end
       end
-       
-       
+
+
     end
   end
 end
